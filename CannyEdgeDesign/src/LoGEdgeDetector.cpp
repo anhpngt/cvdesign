5f965c3dac0a819d4d217352ae942d5a252b3df2
phamngtuananh <phamngtuananh@gmail.com>
2018-03-28 20:23:14 +0800

    FInalize

--- CannyEdgeDesign/src/LoGEdgeDetector.cpp
+++ CannyEdgeDesign/src/LoGEdgeDetector.cpp
@@ -1,3 +1,4 @@
+#include <algorithm>
 #include <bitset>
 #include <cmath>
 #include <fstream>
@@ -32,11 +33,7 @@ Vec2i Vec2dToVec2i(const Vec2d input)
     for(uint16_t y = 0; y < output[0].size(); y++)
     {
       if(input[x][y] > 255) output[x][y] = 255;
-      else if (input[x][y] < 0) 
-      {
-        printf("pixel (%d, %d) of value %.2lf", x, y, input[x][y]);
-        output[x][y] = 0;
-      }
+      else if (input[x][y] < 0) output[x][y] = 0;
       else output[x][y] = (pixel_t)input[x][y];
     }
   return output;
@@ -68,20 +65,22 @@ public:
   ImageProcessing(Vec2i, double);
 
   Vec2i img_src_;
-  Vec2d img_blur_, img_Ix_, img_Iy_, img_I_, img_nms_;
-  Vec2d gauss_mask_;
+  Vec2d img_blur_, img_Ix_, img_LoG_, img_edge_;
+  Vec2d gauss_mask_, log_mask_;
   double sigma_;
 
 private:
   void conv2d(const Vec2d, const Vec2d, Vec2d&);
   double computeGaussianWeight(int x, int y, double sigma);
+  double computeLoGWeight(int x, int y, double sigma);
   Vec2d computeGaussKernel(int k_size, double sigma);
+  Vec2d computeLoGKernel(int size, double sigma);
   // Vec2d mask_dx_ = {{-1, -2, -1},
   //                   { 0,  0,  0},
   //                   { 1,  2,  1}};
   // Vec2d mask_dy_ = {{-1,  0,  1},
   //                   {-2,  0,  2},
-  //                   {-1,  0,  1}}; // easier to visualize
+  //                   {-1,  0,  1}};
   Vec2d mask_dx_ = {{-0.125, -0.25, -0.125},
                     { 0,         0,      0},
                     { 0.125,  0.25,  0.125}};
@@ -89,6 +88,8 @@ private:
                     {-0.25,      0,   0.25},
                     {-0.125,     0,  0.125}};
   double const pi = 3.14159265359;
+  double edge_strength_threshold = 0.0;
+  bool use_kernel_for_log = true;
 };
 
 ImageProcessing::ImageProcessing(Vec2i img, double sigma):
@@ -106,10 +107,8 @@ ImageProcessing::ImageProcessing(Vec2i img, double sigma):
     exit(-1);
   }
   int mask_size = ceil(5 * sigma_);
-  if(mask_size < 3) 
-    mask_size = 3;
-  else if(mask_size % 2 == 0) 
-    mask_size += 1;
+  if(mask_size < 3) mask_size = 3;
+  else if(mask_size % 2 == 0) mask_size += 1;
 
   // Compute mask value
   gauss_mask_ = computeGaussKernel(mask_size, sigma_);  
@@ -123,126 +122,128 @@ ImageProcessing::ImageProcessing(Vec2i img, double sigma):
     std::cout << std::endl;
   }
   std::cout << std::endl;
-    
+
   // Conv2d
   conv2d(Vec2iToVec2d(img_src_), gauss_mask_, img_blur_);
   
   ////////////////////////////////////////
-  //        Compute Derivatives         //
+  //          Compute Laplacian         //
   ////////////////////////////////////////
 
-  conv2d(img_blur_, mask_dx_, img_Ix_);  // dx
-  conv2d(img_blur_, mask_dy_, img_Iy_);  // dy
-  img_I_.resize(img_Ix_.size(), std::vector<double>(img_Ix_[0].size(), 0));
-  for(uint16_t x = 0; x < img_I_.size(); x++)
-    for(uint16_t y = 0; y < img_I_[0].size(); y++)
+  // Laplacian is computed using Sobel operator twice in each direction
+  if(!use_kernel_for_log)
+  {
+    Vec2d img_Ix, img_Ix2, img_Iy, img_Iy2;
+    conv2d(img_blur_, mask_dx_, img_Ix);
+    conv2d(img_Ix, mask_dx_, img_Ix2);
+    conv2d(img_blur_, mask_dy_, img_Iy);
+    conv2d(img_Iy, mask_dy_, img_Iy2);
+
+    img_LoG_.resize(img_Ix2.size(), std::vector<double>(img_Ix2[0].size(), 0));
+    for(uint16_t x = 0; x < img_LoG_.size(); x++)
+      for(uint16_t y = 0; y < img_LoG_[0].size(); y++)
+        img_LoG_[x][y] = img_Ix2[x][y] + img_Iy2[x][y]; // d(dx) + d(dy)
+  }
+  else
+  {
+    log_mask_ = computeLoGKernel(mask_size, sigma_);
+    std::cout << "Laplacian of Gaussian mask: " << std::endl;
+    for(uint16_t i = 0; i < log_mask_.size(); i++)
     {
-      double Ix = img_Ix_[x][y];
-      double Iy = img_Iy_[x][y];
-      img_I_[x][y] = sqrt((Ix * Ix + Iy * Iy) / 2); // sqrt(dx^2 + dy^2)
+      for(uint16_t j = 0; j < log_mask_.size(); j++)
+        printf("  %11.8lf", log_mask_[i][j]);
+      std::cout << std::endl;
     }
+    std::cout << std::endl;
+
+    conv2d(img_blur_, log_mask_, img_LoG_);
+  }
 
   ////////////////////////////////////////
-  //       Non-Maximum Suppression      //
+  //         Find Zero-crossing         //
   ////////////////////////////////////////
-  
-  // // Gradient smoothing with moving average of 3 pixels
-  // for(uint16_t x = 1, x_end = img_I_.size() - 1; x < x_end; x++) 
-  //   for(uint16_t y = 1, y_end = img_I_[0].size() - 1; y < y_end; y++)
-  //   {
-  //     // Compute the gradient direction
-  //     double grad_angle = atan2(img_Iy_[x][y], img_Ix_[x][y]) * 180 / pi; // (-180, 180], note that x->down, y->right
-      
-  //     // Process
-  //     int code = abs(int(round(grad_angle / 45.0))) % 4;
-  //     switch(code)
-  //     {
-  //       case 0: // up <-> down
-  //         img_I_[x][y] = (img_I_[x-1][y] + img_I_[x][y] + img_I_[x+1][y]) / 3.0;
-  //         break;
-  //       case 1: // up-left <-> down-right
-  //         img_I_[x][y] = (img_I_[x+1][y+1] + img_I_[x][y] + img_I_[x-1][y-1]) / 3.0;
-  //         break;
-  //       case 2: // left <-> right
-  //         img_I_[x][y] = (img_I_[x][y-1] + img_I_[x][y] + img_I_[x][y+1]) / 3.0;
-  //         break;
-  //       case 3: // up-right <-> down-left
-  //         img_I_[x][y] = (img_I_[x-1][y+1] + img_I_[x][y] + img_I_[x+1][y-1]) / 3.0;
-  //         break;
-  //       default: 
-  //         std::cout << "ERROR: grad_angle_code = " << code << std::endl;
-  //         exit(-1);
-  //     }
-  //   }
 
-  // Non-Maximum Suppression
-  // note that this will reduce dimensions of img by (2, 2)
-  img_nms_.resize(img_I_.size() - 2, std::vector<double>(img_I_[0].size() - 2, 0));
-  for(uint16_t x = 1, x_end = img_I_.size() - 1; x < x_end; x++) 
-    for(uint16_t y = 1, y_end = img_I_[0].size() - 1; y < y_end; y++)
+  img_edge_.resize(img_LoG_.size() - 2, std::vector<double>(img_LoG_[0].size() - 2, 0));
+  for(uint16_t x = 1, x_end = img_edge_.size() - 1; x < x_end; x++)
+    for(uint16_t y = 1, y_end = img_edge_[0].size() - 1; y < y_end; y++)
     {
-      // Compute the gradient direction
-      double grad_angle = atan2(img_Iy_[x][y], img_Ix_[x][y]) * 180 / pi; // (-180, 180], note that x->down, y->right
-      
-      // Process
-      int code = abs(int(round(grad_angle / 45.0))) % 4;
-      switch(code)
+      // Zero-crossing: 0 value or having opposite sign neighbors
+      if(img_LoG_[x][y] == 0)
       {
-        case 0: // up <-> down
-          if(img_I_[x][y] > img_I_[x-1][y] && img_I_[x][y] > img_I_[x+1][y])
-            img_nms_[x-1][y-1] = img_I_[x][y];
-          break;
-        case 1: // up-left <-> down-right
-          if(img_I_[x][y] > img_I_[x-1][y-1] && img_I_[x][y] > img_I_[x+1][y+1])
-            img_nms_[x-1][y-1] = img_I_[x][y];
-          break;
-        case 2: // left <-> right
-          if(img_I_[x][y] > img_I_[x][y-1] && img_I_[x][y] > img_I_[x][y+1])
-            img_nms_[x-1][y-1] = img_I_[x][y];
-          break;
-        case 3: // up-right <-> down-left
-          if(img_I_[x][y] > img_I_[x-1][y+1] && img_I_[x][y] > img_I_[x+1][y-1])
-            img_nms_[x-1][y-1] = img_I_[x][y];
-          break;
-        default: 
-          std::cout << "ERROR: grad_angle_code = " << code << std::endl;
-          exit(-1);
+        // Assign the biggest gradient of opposite neighbor couple (if they have different signs)
+        double grad_br_tl = img_LoG_[x+1][y+1]*img_LoG_[x-1][y-1] < 0 ? std::fabs(img_LoG_[x+1][y+1] - img_LoG_[x-1][y-1]) : 0;
+        double grad_bb_tt = img_LoG_[x+1][y]*img_LoG_[x-1][y] < 0 ? std::fabs(img_LoG_[x+1][y] - img_LoG_[x-1][y]) : 0;
+        double grad_bl_tr = img_LoG_[x+1][y-1]*img_LoG_[x-1][y+1] < 0 ? std::fabs(img_LoG_[x+1][y-1] - img_LoG_[x-1][y+1]) : 0;
+        double grad_rr_ll = img_LoG_[x][y+1]*img_LoG_[x][y-1] < 0 ? std::fabs(img_LoG_[x][y+1] - img_LoG_[x][y-1]) : 0;
+        double grad_array[] = {grad_br_tl, grad_bb_tt, grad_bl_tr, grad_rr_ll};
+        img_edge_[x][y] = *std::max_element(grad_array, grad_array + 4);
       }
-    }
+      else // look for opposite sign neighbor
+      {
+        double neighbor_array[] = {img_LoG_[x-1][y-1], img_LoG_[x-1][y], img_LoG_[x-1][y+1],
+                                   img_LoG_[x][y-1],                     img_LoG_[x][y+1],
+                                   img_LoG_[x+1][y-1], img_LoG_[x+1][y], img_LoG_[x+1][y+1]};
+        if(img_LoG_[x][y] > 0) // then, find most neg = min
+        {
+          double most_neg_neighbor = *std::min_element(neighbor_array, neighbor_array + 8);
+          if(most_neg_neighbor < 0)
+            img_edge_[x][y] = img_LoG_[x][y] - most_neg_neighbor;
+          else
+            img_edge_[x][y] = 0;
+        }
+        else // then, find most pos = max
+        {
+          double most_pos_neighbor = *std::max_element(neighbor_array, neighbor_array + 8);
+          if(most_pos_neighbor > 0)
+            img_edge_[x][y] = -img_LoG_[x][y] + most_pos_neighbor;
+          else
+            img_edge_[x][y] = 0;
+        }
+      }
 
+      // Filter edge if its strength is below threshold
+      if(img_edge_[x][y] < edge_strength_threshold)
+        img_edge_[x][y] = 0;
+    }
+  
   ////////////////////////////////////////
   //           Post-processing          //
   ////////////////////////////////////////
 
-  // Normalize pixel intensity of img_I_ and img_nms_ for visualization
-  double brightest_pixel_val = 0;
-  for(uint16_t x = 0, x_end = img_I_.size(); x < x_end; x++) 
-    for(uint16_t y = 0, y_end = img_I_[0].size(); y < y_end; y++)
+  // Normalize (0-255 range) pixel intensity of img_LoG_ and img_edge_ for visualization
+  double high_val = -999.0;
+  double low_val = 999.0;
+  for(uint16_t x = 0, x_end = img_LoG_.size(); x < x_end; x++) 
+    for(uint16_t y = 0, y_end = img_LoG_[0].size(); y < y_end; y++)
     {
-      if(img_I_[x][y] > brightest_pixel_val)
-        brightest_pixel_val = img_I_[x][y];
-    }
-  double mul_val = 255.0 / brightest_pixel_val;
-  std::cout << "Multiplying img_I_ by " << mul_val << " to visualize" << std::endl;
-  for(uint16_t x = 0, x_end = img_I_.size(); x < x_end; x++) 
-    for(uint16_t y = 0, y_end = img_I_[0].size(); y < y_end; y++)
-    {
-      img_I_[x][y] = img_I_[x][y] * mul_val;
+      if(img_LoG_[x][y] > high_val)
+        high_val = img_LoG_[x][y];
+      if(img_LoG_[x][y] < low_val)
+        low_val = img_LoG_[x][y];
     }
   
-  brightest_pixel_val = 0;
-  for(uint16_t x = 0, x_end = img_nms_.size(); x < x_end; x++) 
-    for(uint16_t y = 0, y_end = img_nms_[0].size(); y < y_end; y++)
+  double mul_val = 255.0 / (high_val - low_val);
+  std::cout << "Offsetting then multiplying img_LoG_ by " << -low_val << " and " << mul_val << " to visualize." << std::endl;
+  for(uint16_t x = 1, x_end = img_LoG_.size() - 1; x < x_end; x++) 
+    for(uint16_t y = 1, y_end = img_LoG_[0].size() - 1; y < y_end; y++)
     {
-      if(img_nms_[x][y] > brightest_pixel_val)
-        brightest_pixel_val = img_nms_[x][y];
+      img_LoG_[x][y] = img_LoG_[x][y] - low_val;
+      img_LoG_[x][y] = img_LoG_[x][y] * mul_val;
     }
-  mul_val = 255.0 / brightest_pixel_val;
-  std::cout << "Multiplying img_nms_ by " << mul_val << " to visualize" << std::endl;
-  for(uint16_t x = 0, x_end = img_nms_.size(); x < x_end; x++) 
-    for(uint16_t y = 0, y_end = img_nms_[0].size(); y < y_end; y++)
+  
+  high_val = 0;
+  for(uint16_t x = 0, x_end = img_edge_.size(); x < x_end; x++) 
+    for(uint16_t y = 0, y_end = img_edge_[0].size(); y < y_end; y++)
     {
-      img_nms_[x][y] = img_nms_[x][y] * mul_val;
+      if(img_edge_[x][y] > high_val)
+        high_val = img_edge_[x][y];
+    }
+  mul_val = 255.0 / high_val;
+  std::cout << "Multiplying img_edge_ by " << mul_val << " to visualize" << std::endl;
+  for(uint16_t x = 0, x_end = img_edge_.size(); x < x_end; x++) 
+    for(uint16_t y = 0, y_end = img_edge_[0].size(); y < y_end; y++)
+    {
+      img_edge_[x][y] = img_edge_[x][y] * mul_val;
     }
 }
 
@@ -311,6 +312,53 @@ Vec2d ImageProcessing::computeGaussKernel(int k_size, double sigma)
 }
 
 ///////////////////////////////////////////////////////////////////////////////
+double ImageProcessing::computeLoGWeight(int x, int y, double sigma)
+{
+  double rho = -(x * x + y * y) / (2.0 * sigma * sigma);
+  return -(1 + rho) * exp(rho) / (3.14159265359 * sigma * sigma * sigma * sigma);
+}
+
+Vec2d ImageProcessing::computeLoGKernel(int k_size, double sigma)
+{
+  // Check input params
+  if(sigma <= 0)
+  {
+    std::cout << "Invalid sigma value in LoG kernel creation: " << sigma << std::endl;
+    exit(-1);
+  }
+
+  if(k_size % 2 == 0 || k_size < 3)
+  {
+    std::cout << "Invalid k_size value in LoG kernel creation: " << k_size << std::endl;
+    exit(-1);
+  }
+
+  // Compute kernel
+  Vec2d log_kernel;
+  log_kernel.resize(k_size, std::vector<double>(k_size, 0));
+  double weight_sum = 0;
+  int kernel_center = (k_size - 1) / 2;
+  for(int x = 0; x < k_size; x++)
+    for(int y = 0; y < k_size; y++)
+    {
+      log_kernel[x][y] = computeLoGWeight(x - kernel_center, y - kernel_center, sigma);
+      weight_sum += log_kernel[x][y];
+    }
+  
+  // Zero-mean kernel
+  double mean = weight_sum / (k_size * k_size);
+  weight_sum = 0;
+  for(int x = 0; x < k_size; x++)
+    for(int y = 0; y < k_size; y++)
+    {
+      log_kernel[x][y] -= mean;
+      weight_sum += log_kernel[x][y];
+    }
+
+  return log_kernel;
+}
+
+///////////////////////////////////////////////////////////////////////////////
 int main(int argc, char** argv)
 {
   if(argc < 3)
@@ -347,30 +395,29 @@ int main(int argc, char** argv)
 
   // Visualize using OpenCV
   printf("Finished! Visualizing using OpenCV...\n");
-  cv::namedWindow("Canny Edge Detector", cv::WINDOW_AUTOSIZE);
+  cv::namedWindow("LoG Edge Detector", cv::WINDOW_AUTOSIZE);
 
   cv::Mat cv_src = VecToMat(img_src);
   cv::Mat cv_blur = VecToMat(imgproc.img_blur_);
-  cv::Mat cv_I = VecToMat(imgproc.img_I_);
-  cv::Mat cv_nms = VecToMat(imgproc.img_nms_);
+  cv::Mat cv_LoG = VecToMat(imgproc.img_LoG_);
+  cv::Mat cv_edge = VecToMat(imgproc.img_edge_);
 
   // Padding zeros to image edges to concatnate
   int blur_padding = (cv_src.cols - cv_blur.cols) / 2;
-  int i_padding = (cv_src.cols - cv_I.cols) / 2;
-  int nms_padding = (cv_src.cols - cv_nms.cols) / 2;
+  int log_padding = (cv_src.cols - cv_LoG.cols) / 2;
+  int edge_padding = (cv_src.cols - cv_edge.cols) / 2;
   cv::copyMakeBorder(cv_blur, cv_blur, blur_padding, blur_padding, blur_padding, blur_padding, cv::BORDER_CONSTANT, cv::Scalar::all(0));
-  cv::copyMakeBorder(cv_I, cv_I, i_padding, i_padding, i_padding, i_padding, cv::BORDER_CONSTANT, cv::Scalar::all(0));
-  cv::copyMakeBorder(cv_nms, cv_nms, nms_padding, nms_padding, nms_padding, nms_padding, cv::BORDER_CONSTANT, cv::Scalar::all(0));
+  cv::copyMakeBorder(cv_LoG, cv_LoG, log_padding, log_padding, log_padding, log_padding, cv::BORDER_CONSTANT, cv::Scalar::all(0));
+  cv::copyMakeBorder(cv_edge, cv_edge, edge_padding, edge_padding, edge_padding, edge_padding, cv::BORDER_CONSTANT, cv::Scalar::all(0));
 
   cv::Mat visual;
   std::vector<cv::Mat> output_mat;
     output_mat.push_back(cv_src);
     output_mat.push_back(cv_blur);
-    output_mat.push_back(cv_I);
-    output_mat.push_back(cv_nms);
+    output_mat.push_back(cv_LoG);
+    output_mat.push_back(cv_edge);
   cv::hconcat(output_mat, visual);
-  cv::imshow("Canny Edge Detector", visual);
-  cv::imwrite("processed_image.jpg", visual);
+  cv::imshow("LoG Edge Detector", visual);
 
   cv::waitKey(-1);
   return 0;
